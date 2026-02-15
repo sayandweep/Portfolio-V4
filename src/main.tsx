f2a2fd1c6a79464dc7d4aeba94d12ad0f1e3a207
sayandweep <sayandweepbiswas@gmail.com>
2026-02-16 02:47:44 +0530

    160226 247

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './app.tsx'
+import App from './app'
 import Header from './components/header.tsx'
 import Footer from './components/footer.tsx'
 
